4281ff8004e99c90f1afa3eeb6fccc159dab0df8
Jotadog <jvscaravonatti@gmail.com>
2019-06-25 00:59:38 -0300

    Rotas de usuario autenticadas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,11 @@ use App\User;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
