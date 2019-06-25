c5fa45e245c84295b4d04413247dd159ec57447f
Jotadog <jvscaravonatti@gmail.com>
2019-06-25 00:52:37 -0300

    Listagem e exclusão de usuários funcionando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d30fe4ef51.js"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
