effaccc45369c3f72dcdff0db7c90aef073ca8d7
Jotadog <jvscaravonatti@gmail.com>
2019-06-24 22:38:52 -0300

    Upload de imagem funcionando

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,40 +17,22 @@
                             </ul>
                         </div>
                     @endif
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                         @csrf
-                        <?php
-                        //TODO corrigir isto
-                            $array = [
-                                'name' => [ 'Nome Completo' ],
-                                'father_name' => [ 'Nome do Pai' ],
-                                'mother_name' => [ 'Nome da Mãe' ],
-                                'date_of_birth' => [ 'Data de Nascimento', 'date' ],
-                                'register' => [ 'Matrícula' ],
-                                'address' => [ 'Endereço' ],
-                                'cpf' => [ 'CPF' ],
-                                'rg' => [ 'RG' ],
-                                'contact' => [ 'Contato' ],
-                                'email' => [ 'Email', 'email' ],
-                                'password' => [ 'Senha', 'password' ],
-                                'photo' => [ 'Foto', 'file' ]
-                            ];
-
-                            foreach($array as $key => $value): 
-                        ?>
-                            <div class="form-group row">
-                                <label for="<?= $key ?>" class="col-md-3 col-form-label text-md-right">{{ $value[0] }}</label>
-                                <div class="col-md-9">
-                                    <input 
-                                        id="{{ $key }}" 
-                                        class="form-control"
-                                        type="{{ $value[1] ?? 'text' }}" 
-                                        name="{{ $key }}" 
-                                        required 
-                                    />
-                                </div>
-                            </div>   
-                        @endforeach
+                            @foreach ($array as $key => $value)
+                                <div class="form-group row">
+                                    <label for="<?= $key ?>" class="col-md-3 col-form-label text-md-right">{{ $value[0] }}</label>
+                                    <div class="col-md-9">
+                                        <input 
+                                            id="{{ $key }}" 
+                                            class="form-control"
+                                            type="{{ $value[1] ?? 'text' }}" 
+                                            name="{{ $key }}" 
+                                            required 
+                                        />
+                                    </div>
+                                </div> 
+                            @endforeach  
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
